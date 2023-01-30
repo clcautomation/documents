2024838c1cf25f12c2f95aee914af3644f1fb287
Agustin Mayol <agusmayolitos@gmail.com>
2023-01-30 20:26:56 -0300

    añadiendo perfiles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
-let perfiles = [{ id: "fernando", name: "FERNANDO MAYOL", mail: "ventas@camozziweb.com.ar", foto: "../assets/img/fer.jpeg" }, { id: "nazareno", name: "NAZARENO ÁLVAREZ", mail: "ventas@camozziweb.com.ar", foto: "../assets/img/naza.jpeg" }]
+let perfiles = [{ id: "fernando", name: "FERNANDO MAYOL", mail: "ventas@camozziweb.com.ar", foto: "../assets/img/fer.jpeg" }, { id: "nazareno", name: "NAZARENO ÁLVAREZ", mail: "nalvarez@camozziweb.com.ar", foto: "../assets/img/naza.jpeg" }, { id: "josefina", name: "JOSEFINA MAYOL", mail: "administracion@camozziweb.com.ar", foto: "../assets/img/jose.jpeg" }]
 
 // Obteniendo el perfil referenciado en la URL
 
 let urlParams = new URLSearchParams(window.location.search);
 let id = urlParams.get("id");
+console.log(id)
 
 function cargarContenido() {
 
@@ -15,24 +16,32 @@ function cargarContenido() {
 
     // Estableciendo valores
 
+    if (id !== null) {
+        for (let i = 0; i < perfiles.length; i++) {
+            let objeto = perfiles[i];
+            if (objeto.id == id) {
+                console.log("Nombre: " + objeto.name);
+                console.log("Correo: " + objeto.mail);
+                console.log("Foto: " + objeto.foto);
 
-    for (let i = 0; i < perfiles.length; i++) {
-        let objeto = perfiles[i];
-        if (objeto.id == id) {
-            console.log("Nombre: " + objeto.name);
-            console.log("Correo: " + objeto.mail);
-            console.log("Foto: " + objeto.foto);
-
-            nombre_id.innerHTML = objeto.name
-            mail_id.innerHTML = objeto.mail
-            mail_id.href = "mailto:" + objeto.mail
-            foto_id.src = objeto.foto
+                nombre_id.innerHTML = objeto.name
+                mail_id.innerHTML = objeto.mail
+                mail_id.href = "mailto:" + objeto.mail
+                foto_id.src = objeto.foto
 
 
-            break;
+                break;
+            }
         }
-    }
 
+    } else {
+        let objeto = perfiles[0];
+
+        nombre_id.innerHTML = objeto.name
+        mail_id.innerHTML = objeto.mail
+        mail_id.href = "mailto:" + objeto.mail
+        foto_id.src = objeto.foto
+    }
 
 
 }
